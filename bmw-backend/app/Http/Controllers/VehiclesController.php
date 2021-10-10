bd86d131939c8d6e2db0b6f7126301482af75575
Admin <admin@Deepthought-2.local>
2021-10-10 01:34:19 +0100

    added responsiveness

--- bmw-backend/app/Http/Controllers/VehiclesController.php
+++ bmw-backend/app/Http/Controllers/VehiclesController.php
@@ -48,28 +48,4 @@ class VehiclesController extends Controller
         return 'Successfully saved data';
 
     }
-
-    function match (Request $req) 
-    {
-        $param = $req->typeName;
-        $secondParam = $req->model;
-        $firstDate = $req->monthOfConstrFrom;
-        $secondData = $req->monthOfConstrTo;
-        $param = $secondParam . $param;
-        // dd($req->getContent());
-        $param = str_replace(' ', '', $param);
-        $param = str_replace('()', '', $param);
-        dd($param);
-        $yourDate = "2009-07-15 00:00:00+00";
-        $query = DB::table('vehicles')->where('model', strpos($param, 'model'))->get();
-        dd($query, date('Y', strtotime($yourDate)));
-    }
-
-    public function show(Request $request) {
-        //Steps to filter
-        //1. Receive vehicle model as query string
-        //2. Use either query builder or eloquent to perform a db query to select from vehicles base on "typeName" (Use can use a like operator if you want.)
-        //NOTE::In Vehicle model: have a typeName getter that removes any space if any.
-        //3. Return result to front end. 
-    }
 }
